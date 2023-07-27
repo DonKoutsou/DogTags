37a67e8562d734a2d4fec945da82ec59be2a39c7
DonKoutsou <anthonykoutsouradis@gmail.com>
2023-07-27 19:03:29 +0200

    updated to released version

--- scripts/Game/Entities/SP_DogTag_Entity.c
+++ scripts/Game/Entities/SP_DogTag_Entity.c
@@ -30,9 +30,16 @@ class DogTagEntity: GameEntity
 			parent = ChimeraCharacter.Cast(parent);
 			if(parent)
 			{
-				SCR_CharacterIdentityComponent CharID = SCR_CharacterIdentityComponent.Cast(parent.FindComponent(SCR_CharacterIdentityComponent));
-				SCR_CharacterRankComponent RankC = SCR_CharacterRankComponent.Cast(parent.FindComponent(SCR_CharacterRankComponent));
-				charname = RankC.GetCharacterRankName(parent) + " " + CharID.GetIdentity().GetName() + " " + CharID.GetIdentity().GetSurname();
+				SCR_CharacterIdentityComponent IdentityComponent = SCR_CharacterIdentityComponent.Cast(parent.FindComponent(SCR_CharacterIdentityComponent));
+				SCR_CharacterRankComponent RankComponent = SCR_CharacterRankComponent.Cast(parent.FindComponent(SCR_CharacterRankComponent));
+				if(IdentityComponent && RankComponent)
+				{
+					Identity ID = IdentityComponent.GetIdentity();
+					if(ID)
+					{
+						charname = RankComponent.GetCharacterRankNameShort(parent) + " " + ID.GetFullName();
+					}
+				}
 			}
 		}
 		if (charname)
